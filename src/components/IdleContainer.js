677705bda359a8b7fd0e70cee411447e6f9f5c9c
Tollefsen <havard.tollefsen@gmail.com>
2019-05-14 21:49:50 +0200

    New direction for the game. Hope to embrase the TOWN part of idle
    The new direction is about resource management, and recruiting villagers to live in your settlement/town.
    One day...

--- src/components/IdleContainer.js
+++ src/components/IdleContainer.js
@@ -1,125 +1,129 @@
 import * as React from 'react'
-import { HiddenUntil, HiddenAfter, ShowBetween, IdleToolWrapper } from '../common/wrappers'
-import { Progressbar, PrimaryButton, ScoreDisplay, SecondaryButton } from '../common/components'
 
-const defaultState = {
-    score: 0,
-    intervalId: 0,
-    incrementRate: 1,
-    upgradeCost: 100,
-    scoreShown: false,
-    firstToolShown: false,
-    secondToolShown: false,
-    toolIntervalId: 0,
-    progbar: 0,
-    hideAuto: false,
-    numberAutomachines: 1,
-    upgradeAutoCost: 500
+const initGameState = {
+    version: 0.3,
+    intervals: {
+        update: 0,
+        save: 0
+    },
+    player: {
+        name: 'NAVN',
+        hp: 10,
+        maxHp: 10,
+        energy: 10,
+        maxEnergy: 10,
+        talents: []
+    },
+    resources: {
+        wood: 0,
+        stone: 0,
+        metal: 0,
+        gold: 0,
+        villagers: 0
+    },
+    generators: {
+        wood: {
+            active: false,
+            rate: 1000,
+            output: 1
+        },
+        stone: {
+            active: false,
+            rate: 1000,
+            output: 1
+        },
+        metal: {
+            active: false,
+            rate: 1000,
+            output: 1
+        },
+        gold: {
+            active: false,
+            rate: 1000,
+            output: 1
+        },
+        villagers: {
+            active: false,
+            rate: 1000,
+            output: 1
+        }
+    }
 }
-
-const toolStyle = {
-    paddingTop: 20,
-    display: 'flex',
-    justifyContent: 'center'
-}
-
 
 class IdleContainer extends React.Component {
     constructor() {
         super()
-        var savegame = JSON.parse(localStorage.getItem("idletown"));
+        const savegame = JSON.parse(localStorage.getItem("idletown"));
         
-        this.state = (savegame) ? savegame : defaultState
-        this.increaseScore = this.increaseScore.bind(this)
-        this.decreaseScore = this.decreaseScore.bind(this)
+        this.state = (savegame) ? savegame : JSON.parse(JSON.stringify(initGameState))
+        
         this.save = this.save.bind(this)
         this.reset = this.reset.bind(this)
+        this.update = this.update.bind(this)
+
+        this.startGenerator = this.startGenerator.bind(this)
     }
 
     componentDidMount() {
-        const intervalId = setInterval(this.save, 200)
+        const updateId = setInterval(this.update, 200)
+        const saveId = setInterval(this.save, 5000)
         this.setState(state => {
-            state.intervalId = intervalId
+            state.intervals.update = updateId
+            return state
+        })
+    }
+
+    update() {
+        /* code that updates */
+        const generators = this.state.generators
+        const oldResources = this.state.resources
+        let newResources = oldResources
+
+        Object.keys(oldResources).forEach(key => {
+            if (generators[key].active) newResources[key] = oldResources[key] + generators[key].output
+        });
+        this.setState(state => {
+            state.resources = newResources
             return state
         })
     }
 
     componentWillUnmount() {
-        clearInterval(this.state.intervalId)
+        Object.keys(this.state.intervals).forEach(key => {
+            clearInterval(this.state.intervals[key])
+        })
     }
 
-
-
-    increaseScore(n) {
-        this.setState(state =>  Object.assign({}, state, {score: state.score + n}))
-        if(this.state.score > 9) this.setState(state =>  Object.assign({}, state, {scoreShown: true}))
-        if(this.state.score > 49) this.setState(state =>  Object.assign({}, state, {firstToolShown: true}))
-        if(this.state.score > 199) this.setState(state =>  Object.assign({}, state, {secondToolShown: true}))
-    }
-
-    decreaseScore(n) {
-        this.setState(state =>  Object.assign({}, state, {score: state.score - n}))
-    }
-
-    increaseIncrementRate(n) {
-        if (this.state.score < this.state.upgradeCost) return;
-        this.setState(state => Object.assign({}, state, {incrementRate: state.incrementRate + n, score: state.score - state.upgradeCost, upgradeCost: state.upgradeCost + 10}))
-    }
-
-    startAuto() {
-        this.setState(state => Object.assign({}, state, {hideAuto: true}))
-        setInterval(() => {
-            this.setState(state => {
-                state.score = (state.progbar == 100) ? state.score + (state.incrementRate * state.numberAutomachines) : state.score
-                state.progbar = (state.progbar < 100) ? state.progbar + 1 : 0
-                return state
-            })
-        }, 50)
-    }
-
-    upgradeAuto() {
-        if (this.state.score < this.state.upgradeAutoCost) return;
-        this.setState(state =>  Object.assign({}, state, {numberAutomachines: state.numberAutomachines + 1, score: state.score - state.upgradeAutoCost}))
+    reset() {
+        this.setState(state => {
+            state = JSON.parse(JSON.stringify(initGameState))
+            return state
+        })
     }
 
     save() {
         localStorage.setItem('idletown', JSON.stringify(this.state));
     }
 
-    reset() {
+
+    startGenerator(element) {
         this.setState(state => {
-            state = defaultState
-            return state
+            Object.keys(state.generators).forEach(key => state.generators[key].active = false)
+            state.generators[element].active = true
+            return true
         })
     }
 
     render() {
-        const primaryButton = (<PrimaryButton onClick={() => this.increaseScore(this.state.incrementRate)}>
-            <HiddenAfter score={this.state.score} target={1}>Press me</HiddenAfter>
-            <ShowBetween score={this.state.score} from={1} until={2}>Again</ShowBetween>
-            <ShowBetween score={this.state.score} from={2} until={3}>Keep going!</ShowBetween>
-        </PrimaryButton>)
-        const secondaryButton = (<SecondaryButton onClick={() => this.increaseIncrementRate(1)}>Cost: {this.state.upgradeCost}</SecondaryButton>)
+        const {wood, stone, metal, gold} = this.state.resources
+
         return (
             <div>
-                <ScoreDisplay score={this.state.score} shown={this.state.scoreShown}/>
-                {primaryButton}
-                <div style={toolStyle}>
-                    <IdleToolWrapper shown={this.state.firstToolShown}>
-                        Rate: {this.state.incrementRate}
-                        <Progressbar score={this.state.score} ceil={this.state.upgradeCost} />
-                        {secondaryButton}
-                    </IdleToolWrapper>
-                    <IdleToolWrapper shown={this.state.secondToolShown}>
-                        #{this.state.numberAutomachines}
-                        <Progressbar score={this.state.progbar} ceil={100} />
-                        <SecondaryButton onClick={() => this.upgradeAuto()}>Cost: {this.state.upgradeAutoCost}</SecondaryButton>
-                        <SecondaryButton onClick={() => this.startAuto()} shown={this.state.hideAuto}>Start</SecondaryButton>
-                    </IdleToolWrapper>
-
-                </div>
-                
-                <button onClick={() => this.reset()}>Reset</button>
+                Wood: {wood}
+                <button onClick={() => this.startGenerator('wood')}>Start wood</button>
+                Stone: {stone}
+                <button onClick={() => this.startGenerator('stone')}>Start stone</button>
+                <button onClick={this.reset}>Reset</button>
             </div>
         )
     }
